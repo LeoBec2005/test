5d8c171500bd996f26dd70ea2be5da52bac1720e
Leonardo Sebastian Becerra Reymer <135758817+LeoBec2005@users.noreply.github.com>
2024-06-15 13:51:44 -0500

    Add files via upload

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,33 +1,125 @@
 #include <iostream>
 using namespace std;
+#include <algorithm>
+#include <vector>
 
-int seq(int arr[], int n) {
 
-	int* p = arr;
-	int* q = arr + n-1;
-	int count = 0;
-	int prev = 2147483647;
+template <typename T>
+class interval {
+private:
+	T first;
+	T end;
 
-	while ((p != q) || (*p >= prev) || (*q >= prev)) {
-		if ((*p < *q) && (*p < prev)) {
-			p++;
-			count++;
-			prev = *p;
-		}
-		if ((*q < *p) && (*q < prev)) {
-			q--;
-			count++;
-			prev = *q;
-		}
+public:
+	interval(const T& a,const T& b) {
+		first = a;
+		end = b;
 	}
-	return count;
+
+	bool operator ()(const T& value) const {
+		return ((value >= first) && (value <= end));
+	}
+};
+
+
+
+class date {
+	private:
+		int day;
+		int month;
+		int year;
+	public:
+		date(int a, int b, int c) {
+			day = a;
+			month = b;
+			year = c;
+		}
+		friend ostream& operator<<(ostream& os, const date& dt);
+
+		bool operator <= (const date& f1) const{
+			if (year <= f1.year) {
+				if (month <= f1.month) {
+					if (day <= f1.day) {
+						return 1;
+					}
+				}
+			}
+			return 0;
+		}
+
+		bool operator >= (const date& f1) const {
+			if (year >= f1.year) {
+				if (month >= f1.month) {
+					if (day >= f1.day) {
+						return 1;
+					}
+				}
+			}
+			return 0;
+		}
+
+		bool operator = (const date& f1) const {
+			return ((year == f1.year) && (month == f1.month) && (day == f1.day));
+		}
+
+		
+		
+
+};
+
+ostream& operator<<(ostream& os, const date& f1) {
+	cout << f1.day << "," << f1.month << "," << f1.year;
+	return os;
 }
 
+class EsMayor {
+	public:
+		EsMayor(int v) : valor(v) {}
+		bool operator () (int otroValor) const {
+		return otroValor > valor;
+		
+	}
+	
+	private:
+		int valor;
+};
+
+template <typename T>
+class Greater_Than {
+public:
+	bool operator () (const T & left, const T &right) {
+		return left >= right;
+	}
+};
+
 int main() {
-	int arr[] = { 4,3,1,5,2 };
+	interval <int> I(2, 8);
+	cout << I(5) << endl;
 
-	cout << seq(arr, 5) << endl;
+	date f1(2, 4, 2006);
+	date f2(2, 7, 2006);
+	date f3(2, 7, 2010);
+	date f4(2, 4, 2006);
 
+	cout << (f1 <= f2) << endl;
+	cout << (f1 >= f2) << endl;
+	cout << (f1 = f4) << endl;
+
+	
+	vector <date > int_nums = { f1,f2,f3,f4 };
+		
+	sort(int_nums.begin(), int_nums.end(),Greater_Than <date >());
+
+
+	for (auto num : int_nums) {
+	cout << num << "; ";
+	
+	}
 
 	return 0;
 }
+
+
+
+
+
